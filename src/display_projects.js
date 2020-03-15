1a4ae3d1c50c852da5987f77f812dd7246e156ff
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-03-15 22:05:18 +0900

    updated module to show current projects

---
+++ src/display_projects.js
@@ -0,0 +1,22 @@
+const projects = () => {
+  const div = document.createElement("div");
+  for (let i = 0; i < window.localStorage.length; i++) {
+    const project = JSON.parse(window.localStorage.getItem(localStorage.key(i)));
+    const tab = document.createElement("div");
+    tab.classList.add('tabs');
+    tab.classList.add("project-tab");
+    div.appendChild(tab);
+    const input = document.createElement("input");
+    input.type = "radio";
+    input.name = "project-tabs";
+    input.id = `${project.name}`;
+    tab.appendChild(input);
+    const label = document.createElement("label");
+    label.innerHTML = `${project.name}`;
+    label.htmlFor = `${project.name}`;
+    tab.appendChild(label);
+  }
+  return div;
+};
+
+export { projects };
