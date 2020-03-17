f28d5e408cb57b819950137c72599c4ec00b120d
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-03-17 21:27:03 +0900

    updated show

---
+++ src/show.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const showForm = () => {
+  const panel = document.querySelector(".form");
+  const task = document.querySelector(".task-container");
+  const taskButton = document.querySelector(".home-task");
+  const taskList = document.querySelector(".task-list-container");
+  document.querySelector("#date").valueAsDate = new Date();
+
+  function showDisplay() {
+    if (panel.style.width) {
+      task.style.display = "none";
+      taskList.style.display = "none";
+    } else {
+      task.style.display = "flex";
+      taskList.style.display = "flex";
+    }
+  }
+
+  if (panel.style.width) {
+    panel.style.width = null;
+    taskButton.innerHTML = "+ Task";
+    panel.addEventListener("transitionend", showDisplay);
+  } else {
+    taskList.style.display = "none";
+    task.style.display = "none";
+    taskButton.innerHTML = "+ Hide";
+    panel.style.width = "100%";
+  }
+};
+
+export { showForm };
